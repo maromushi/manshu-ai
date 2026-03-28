ad69e6cf596dceb901ebcc5608c14a7b47d1babb
maromushi <maromushi@gmail.com>
2026-03-28 17:45:55 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -1340,7 +1340,7 @@ if st.button("計算"):
                     DoubleAttackScore > 0.08
                     and Start[5] >= Start[3] - 0.01
                 ):
-                    SecondAdj[5] *= 1.12
+                    SecondAdj[5] *= 1.15
 
             if NoAttackFlag == 1:
                 
