eb4f466a40989230664c4e1ffd1ecaccf4557b3c
maromushi <maromushi@gmail.com>
2026-04-06 14:51:00 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -3095,7 +3095,7 @@ if st.button("計算"):
                     
                     results.append((boats[a],boats[b],boats[c],p))
 
-        return results, ChaosScore, P1, DoubleAttackScore, InsideSurvival, debug_log
+        return results, ChaosScore, P1, DoubleAttackScore, InsideSurvival, debug_log, StrongAttack, MidAttack
     # =====================================
     # 進入パターン
     # =====================================
@@ -3108,8 +3108,9 @@ if st.button("計算"):
         order_ex=[0,1,2,3,4,5]
 
     try:
-        res_waku, chaos1, P1_waku, DAS1, IS1, debug_log = run_ai(order_waku)
-        res_ex, chaos2, P1_ex, DAS2, IS2, debug_log = run_ai(order_ex)
+        res_waku, chaos1, P1_waku, DAS1, IS1, debug_log, StrongAttack_waku, MidAttack_waku = run_ai(order_waku)
+
+        res_ex, chaos2, P1_ex, DAS2, IS2, debug_log, StrongAttack_ex, MidAttack_ex = run_ai(order_ex)
     
     except Exception as e:
         import traceback
@@ -3268,7 +3269,7 @@ if st.button("計算"):
             mark = "○"
     
         # ▲は攻め条件＋上位6まで
-        elif i <= 5 and (MidAttack or StrongAttack) and head >= 2:
+        elif i <= 5 and (MidAttack_ex or StrongAttack_ex) and head >= 2:
             mark = "▲"
     
         else:
