fdc5f960ebabf3aa71e3c196e8ec240d348e7180
maromushi <maromushi@gmail.com>
2026-04-08 17:53:37 +0900

    app.py を更新

--- app.py
+++ app.py
@@ -948,7 +948,16 @@ if st.button("計算"):
         elif race_type == "no_attack":
 
             FS_mult[0] *= 1.12
-            FS_mult[1] *= 0.88
+            # 無風＋差し弱いときだけ2を削る
+            if (
+                NoAttackFlag == 1
+                and CPI[1] < CPI[0] - 0.03
+                and Start[1] > Start[0]
+            ):
+                FS_mult[1] *= 0.82
+            else:
+                FS_mult[1] *= 0.92
+                
             FS_mult[2] *= 0.95
         
             FS_mult[3] *= 0.70   # ★修正（重要）
