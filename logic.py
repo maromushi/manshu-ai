17191474811d9dbf8bd534fbce01f5b059c9782d
maromushi <maromushi@gmail.com>
2026-05-02 22:13:09 +0900

    logic.py を更新

--- logic.py
+++ logic.py
@@ -566,3 +566,17 @@ def merge(P_no, P_weak, P_at, state):
         )
 
     return normalize_sum(P)
+    
+    
+    # ===============================
+    # 出力用
+    # ===============================
+def run_ai(data, venue):
+    f = calc_features(data)
+    state = detect_state(f)
+    P_no = sim_no_attack(f)
+    P_weak = sim_weak(f)
+    P_at = sim_attack(f)
+    P = merge(P_no, P_weak, P_at, state)
+
+    return P, state
